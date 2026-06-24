cd377d4d312c32f771d6ccb11ae259bc16e25fbb
chinmayd1 <chinmay.minskole@gmail.com>
2026-06-24 21:27:46 +0530

    added

--- utils/apiUtils/ReponseValidator.ts
+++ utils/apiUtils/ReponseValidator.ts
@@ -2,21 +2,42 @@ import {expect} from '@playwright/test'
 
 export class ReponseValidator {
 
-    static validateStatus(actual:number,expected:number):void{
-        expect(actual).toBe(expected)
+
+    static validateStatus(actualStatus:number,expectedStatus:number):void{
+        expect(actualStatus).toEqual(expectedStatus)
     }
 
-
-    // {
-    //     claim:234,
-    //     claimref:"5435"
-    // }
-
-    static validateProperty(responseBody:any,properName:string):void{
-        expect(responseBody).toHaveProperty(properName)
+    static validateProperty(responseBody:any,propertyName:string):void{
+        expect(responseBody).toHaveProperty(propertyName)
     }
 
+    static validateValue(actualValue:any,expectedValue:any){
+        expect(actualValue).toBe(expectedValue)
+    }
 
+    static validateContains(actualText:string,expectedText:string){
+        expect(actualText).toContain(expectedText)
+    }
+
+    static validateNotNull(value:any):void{
+        expect(value).not.toBeNull
+    }
+
+    static validateArraySize(array:any[],expectedSize:number):void{
+        expect(array.length).toBe(expectedSize)
+    }
+
+     static validatePerson(array:any[],expectedName:string):void{
+        //expect(array.length).toBe(expectedSize)
+        let obj = array.filter(function(el){
+            return el.firstName = expectedName
+        })
+        expect(obj).not.toBeUndefined
+        // validate complete object
+    }
 
 
 }
+
+// API ===> call values ====> API salesforces call ===> values ====> 
+
